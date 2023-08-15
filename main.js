6def302fbd8ff7fa2565a9fb960b6a3ce54f28bf
Wikaobl <wikob@icloud.com>
2023-08-15 11:48:35 +0200

    fixing mistakes in js file

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const price = document.querySelector("#price");
 const people = document.querySelector("#people");
 const tip = document.querySelector("#tip");
-const btn = document.querySelector(".count");
 const error = document.querySelector(".error");
+const countBtn = document.querySelector(".count");
 const costInfo = document.querySelector(".cost-info");
 const cost = document.querySelector(".cost");
 
-checkInput = () => {
-  if (price.value === "" || people.value === "" || tip.value == 0) {
-    error.textContent = "uzupełnij wszystkie pola!";
+const showBill = () => {
+  if (price.value == "" || people.value == "" || tip.value == 0) {
+    error.textContent = "Uzupełnij wszystkie pola!";
+    costInfo.style.display = "none";
   } else {
     error.textContent = "";
     countBill();
   }
 };
 
-countBill = () => {
+const countBill = () => {
   const newPrice = parseFloat(price.value);
   const newPeople = parseInt(people.value);
   const newTip = parseFloat(tip.value);
-  const sum = newPrice + (newPrice * newTip) / newPeople;
+  const sum = (newPrice + newPrice * newTip) / newPeople;
+
   costInfo.style.display = "block";
   cost.textContent = sum.toFixed(2);
 };
 
-btn.addEventListener("click", checkInput);
+countBtn.addEventListener("click", showBill);
